fd443ca7b71924215028e976df450d4c339c2dfb
Janet <jyctech15@gmail.com>
2022-02-28 15:40:26 -0800

    completed 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (startNum, endNum) {
+  if (!Number.isInteger(startNum) || !Number.isInteger(endNum)) {
+    return "ERROR";
+  } else {
+    if (startNum < 0 || endNum < 0) {
+      return "ERROR";
+    }
+  }
 
+  let sum = 0;
+  if (startNum > endNum) {
+    for (let i = startNum; i >= endNum; i--) {
+      sum += i;
+    }
+  } else {
+    for (let i = startNum; i <= endNum; i++) {
+      sum += i;
+    }
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,19 @@
-const ftoc = function() {
+const ftoc = function (temp) {
+  let newTemp = parseFloat(((temp - 32) * (5 / 9)).toFixed(1));
 
+  return newTemp;
 };
 
-const ctof = function() {
+const ctof = function (temp) {
+  let newTemp = parseFloat((temp * (9 / 5) + 32).toFixed(1));
 
+  return newTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
+
+// [°F] = [°C] × 9⁄5 + 32	[°C] = ([°F] − 32) × 5⁄9
